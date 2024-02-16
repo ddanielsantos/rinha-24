dd6f12944f859627b738678cb7a99f539cea4455
ddanielsantos <80872981+ddanielsantos@users.noreply.github.com>
2024-02-15 22:00:33 -0300

    feat: allow more db connections

--- src/db.rs
+++ src/db.rs
@@ -3,7 +3,7 @@ use sqlx::postgres::PgPool;
 
 pub async fn create_connection_pool(database_url: &String) -> PgPool {
     PoolOptions::new()
-        .max_connections(5)
+        .max_connections(20)
         .connect(database_url)
         .await.expect("Failed to connect to the database")
 }
